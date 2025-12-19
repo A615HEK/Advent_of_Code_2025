17bd7aebb742b700827de8243f3a5438fbb34fb0
A615HEK <abishek05072004@gmail.com>
2025-12-19 02:19:25 +0000

    D2P1 - Done; D1P2 - Incomplete

---
+++ Day1_Part2_2025.py
@@ -0,0 +1,34 @@
+# No change in puzzle input
+input_file_path = "./Inputs/day1_input.txt"
+with open(input_file_path) as input_file:
+    input_file = input_file.readlines()
+# Sample inputs before real test
+# input_file = ["R1000\n"]
+# input_file = ["L49\n", "L101\n"]
+# input_file = ["L68\n", "L30\n", "R48\n", "L5\n", "R60\n", "L55\n", "L1\n", "L99\n", "R14\n", "L82\n"]
+current_num = 50
+num_of_zeros = 0
+for command in input_file:
+    direction_to_turn = command[0]
+    num_of_turns = int(command[1:-1])
+    prev_num = current_num
+    if direction_to_turn == "R":
+        current_num = (current_num + num_of_turns)
+    elif direction_to_turn == "L":
+        current_num = (current_num - num_of_turns)
+    if ((current_num < 0) and (prev_num>0)) or abs(current_num)>99:
+        if abs(current_num) > 99:
+            num_of_zeros += abs(current_num)//100
+            if current_num < 0:
+                num_of_zeros += 1
+        else:
+            num_of_zeros += 1
+        if current_num%100 == 0:
+            num_of_zeros -= 1
+    # Get mod100 to bound value between 0 - 99
+    current_num%=100
+    if current_num == 0:
+        num_of_zeros += 1
+print(f"The answer is : {num_of_zeros}")
+print(f"Current number is : {current_num}")
+#  - Got it in da _ try!!!
--- Day2_2025.py
+++ Day2_2025.py
@@ -1,31 +1,50 @@
-# No change in puzzle input
-input_file_path = "./Inputs/day1_input.txt"
+input_file_path = "./Inputs/day2_input.txt"
 with open(input_file_path) as input_file:
-    input_file = input_file.readlines()
-# Sample inputs before real test
-input_file = ["R1000\n"]
-# input_file = ["L68\n", "L30\n", "R48\n", "L5\n", "R60\n", "L55\n", "L1\n", "L99\n", "R14\n", "L82\n"]
-current_num = 50
-num_of_zeros = 0
-for command in input_file:
-    direction_to_turn = command[0]
-    num_of_turns = int(command[1:-1])
-    prev_num = current_num
-    if direction_to_turn == "R":
-        current_num = (current_num + num_of_turns)
-    elif direction_to_turn == "L":
-        current_num = (current_num - num_of_turns)
-    if ((current_num < 0) and (prev_num>0)) or abs(current_num)>99:
-        if abs(current_num) > 99:
-            num_of_zeros += abs(current_num)//100
-        else:
-            num_of_zeros += 1
-        if current_num%100 == 0:
-            num_of_zeros -= 1
-    # Get mod100 to bound value between 0 - 99
-    current_num%=100
-    if current_num == 0:
-        num_of_zeros += 1
-print(f"The answer is : {num_of_zeros}")
-print(f"Current number is : {current_num}")
-#  - Got it in da _ try!!!
+    input_file = input_file.readline()
+    input_file = input_file.split(",")
+# Sample inputs
+# input_file = ["11-22","95-115","998-1012","1188511880-1188511890","222220-222224", "1698522-1698528", "446443-446449", "38593856-38593862","565653-565659", "824824821-824824827", "2121212118-2121212124"]
+persistent_sum = 0
+for input_range in input_file:
+    underscore_index = input_range.index("-")
+    start_id = input_range[:underscore_index]
+    stop_id = input_range[underscore_index+1:]
+    start_size_to_check, stop_size_to_check = 0, 0
+    if len(start_id)%2 == 0:
+        start_size_to_check = len(start_id)//2
+    if len(stop_id)%2 == 0:
+        stop_size_to_check = len(stop_id)//2
+    if start_size_to_check:
+        print("Yes to start size...")
+        start_half = int(start_id[:start_size_to_check])
+        start_checker = start_half+(start_half*(10**start_size_to_check))
+        if int(start_id) < start_checker:
+            start_id = start_checker
+    elif 10**(len(start_id)) <= int(stop_id):
+        print("New starters possible...")
+        start_size_to_check = (len(start_id)//2)
+        start_half = 10**(start_size_to_check)
+        start_checker = start_half+(start_half*(10**(start_size_to_check+1)))
+        start_size_to_check += 1
+        if int(start_id) < start_checker:
+            start_id = start_checker
+    if not start_size_to_check:
+        continue
+    if stop_size_to_check:
+        print("Yes to stop size...")
+        stop_half = int(stop_id[:stop_size_to_check])
+        stop_checker = stop_half+(stop_half*(10**stop_size_to_check))
+        if int(stop_id) > stop_checker:
+            stop_id = stop_checker
+    print("Going to the loop...")
+    print(f"IDs: {(start_id, stop_id)}")
+    for num_to_be_checked in range(int(start_id), int(stop_id)+1):
+        if int(str(num_to_be_checked)[:start_size_to_check])>start_half:
+            start_half += 1
+            start_size_to_check = len(str(start_half))
+            start_checker = start_half+(start_half*(10**start_size_to_check))
+        if num_to_be_checked == start_checker:
+            print((num_to_be_checked, start_checker))
+            persistent_sum += num_to_be_checked
+print(persistent_sum)
+# 56660955519 - Got it in da 1st try!!!
